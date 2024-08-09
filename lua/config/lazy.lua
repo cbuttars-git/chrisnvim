4393d7a2378f62990854da601741e2f7dc355487
Christopher S. Buttars <chrisbuttars@live.com>
2024-08-09 16:48:01 -0500

    Integrated existing laxyvim into my custom neovim.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,9 +17,10 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { dir = "~/git-repositories/github/cbuttars-git/lazyvim", import = "lazyvim.plugins" },
     -- import/override with your plugins
     { import = "plugins" },
+    dev = true,
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
@@ -30,7 +31,11 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  --  dev = {
+  --    path = "~/git-repositories/github/cbuttars-git/projects/lazyvim",
+  --    patterns = { "LazyVim", "cbuttars-git/LazyVim" },
+  --  },
+  install = { colorscheme = { "catppuccin", "mocha" } },
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
